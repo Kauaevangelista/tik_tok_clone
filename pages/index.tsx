c53d5ca10cf02bf739551ee30c1432b931c24fcd
Kauaevangelista <kaua13evangelista@gmail.com>
2023-05-22 21:34:14 -0300

    try

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,41 @@
-import type { NextPage } from 'next'
+import React from 'react';
+import axios from 'axios';
 
-const Home: NextPage = () => {
-  return (
-    <h1 className='underline text-3xl'>
-      Tik Tik
-    </h1>
-  )
+import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
+
+interface IProps {
+  videos: Video[];
 }
 
-export default Home
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`https://localhost:3000/api/post`);
+
+  if(topic) {
+    response = await axios.get(`https://localhost:3000/api/discover/${topic}`);
+  }
+  
+  return {
+    props: { videos: response.data },
+  };
+};
